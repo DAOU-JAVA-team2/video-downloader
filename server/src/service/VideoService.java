031e50bac0e1ad04e1070ed6286ea9a3d57f5cff
조준하 <whwnsgk830@naver.com>
2024-03-29 00:47:28 +0900

    feat : merge videoAdd & favoriteAdd

--- server/src/service/VideoService.java
+++ server/src/service/VideoService.java
@@ -1,7 +1,10 @@
 // video controller
 package service;
 
+import dao.FavoriteDAOImpl;
+import dao.UserDAOImpl;
 import dao.VideoDAOImpl;
+import dto.FavoriteDTO;
 import dto.VideoDTO;
 import network.Request;
 import network.Response;
@@ -11,10 +14,14 @@ public class VideoService {
     private Response response;
     private final VideoDTO videoDTO;
     private final VideoDAOImpl dbUtil;
+    private final FavoriteDAOImpl fDao;
+    private final UserDAOImpl uDao;
 
     // constructor
     public VideoService() throws Exception {
         this.dbUtil = new VideoDAOImpl();
+        this.fDao = new FavoriteDAOImpl();
+        this.uDao = new UserDAOImpl();
         this.videoDTO = new VideoDTO();
         this.response = new Response();
     }
@@ -23,15 +30,24 @@ public class VideoService {
     public Response videoAdd(Request request) throws Exception {
         videoDTO.setTitle(request.get("title"));
         videoDTO.setUrl(request.get("url"));
-        int result = dbUtil.insertVideo(videoDTO);
+        int video_id = dbUtil.insertVideo(videoDTO);
 
         response = new Response();
-        response.put("msg", result != -1 ? "Success" : "Failed");
-        if(result != -1){
-            response.put("videoId", String.valueOf(result));
+        response.put("msg", video_id != -1 ? "Success" : "Failed");
+        try {
+            if(video_id != -1){
+                response.put("videoId", String.valueOf(video_id));
+                FavoriteDTO fDto = new FavoriteDTO();
+                Integer user_id = uDao.getUserIdByAccess(request.get("access")).getUser_id();
+                fDto.setUser_id(user_id);
+                fDto.setVideo_id(video_id);
+                fDao.addFavorite(fDto);
+            }
+            response.put("select", "video/add");
         }
-        response.put("select", "video/add");
-
+        catch (Exception e) {
+            e.printStackTrace();
+        }
         return response;
     }
     public Response videoDelete(Request request) throws Exception {
